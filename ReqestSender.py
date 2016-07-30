472f26add2d8342781100096422b0a1d345b233a
always-smiling <light.apple.juice@gmail.com>
2016-07-31 01:52:26 +0300

    Upd requestSender

--- ReqestSender.py
+++ ReqestSender.py
@@ -66,17 +66,50 @@ config = settings()
 #     fid.close()
 
 class RequestSender:
+
      def __init__(self):
          self.logger = Commands.workWithLog(config.log)
          self.matching = dict()
+         self.CNNstyles = [];
          self.loadMatching()
 
      def loadMatching(self):
          with io.open(config.database_name) as file:
              for idx, line in enumerate(file):
-                 lineSplit = line.split(' ')
-                 self.matching[str(idx)] = lineSplit[1:len(lineSplit)]
+                 lineSplit = line.strip('\n').split(' ')
+                 self.CNNstyles.append(str(lineSplit[0]))
+                 self.matching[str(lineSplit[0])] = lineSplit[1:len(lineSplit)]
+         return
+
+     def parseVector(self, vector):
+         #parsing
+         targetStyle = None
+         for idx, value in enumerate(vector):
+             elem = self.matching[self.CNNstyles[idx]]
+             minValue = 0.0#elem[0]
+             maxValue = 1.0#elem[1]
+             if (value > minValue) & (value < maxValue):
+                 targetStyle = elem[2]
+                 break
+         return targetStyle
+
+     def sendRequest(self, style):
+         params = {'values': str(style), 'size': 100}
+         try:
+            r = requests.post("http://muzis.ru/api/stream_from_values.api", data=params)
+            parsed_string = r.json()
+
+            song = parsed_string['songs'][0]['lyrics']
+            lyrics = song.encode('utf-8')
+         except:
+             self.logger.siteWarning()
+         return
 
 RS = RequestSender()
+targetStyle = RS.parseVector([0.02495627,  0.01625971, 0.05735248,  0.03012436, 0.01382483,  0.01830654,
+  0.02935727,  0.02657287,  0.02752874,  0.00897765,  0.01500904,  0.01215811,
+  0.02771803,  0.23560295,  0.01554973,  0.01857543, 0.03318637,  0.17324899,
+  0.18867876,  0.02701183])
+RS.sendRequest(targetStyle)
 
 
